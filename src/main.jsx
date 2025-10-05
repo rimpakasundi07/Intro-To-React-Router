2486b6c4e9961a662ff0bcebce81e4d32f5c7428
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-05 22:57:12 +0530

    add React Router configuration with multiple routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
   {
@@ -13,10 +13,31 @@ const router = createBrowserRouter([
     path: "about",
     element: <div>About me here</div>,
   },
+  {
+    path: "blogs",
+    element: <div>All my blogs are here</div>,
+  },
+  {
+    path: "blogs",
+    element: <div>All my blogs are here</div>,
+  },
+  {
+    path: "app",
+    Component: App,
+  },
+  {
+    path: "/app2",
+    // element: <App></App>
+    Component: App,
+  },
+  {
+    path: "*",
+    element: <h3>Not Found: 404 Status</h3>,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>
 );
