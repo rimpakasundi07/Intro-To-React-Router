30b7e252b5c5bfe744b0356ca62269f3ded1a359
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-06 11:59:51 +0530

    add routing structure with nested routes and 404 handling

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./componets/Root/Root.jsx";
+import Home from "./componets/Home/Home.jsx";
+import Mobiles from "./componets/Mobiles/Mobiles.jsx";
+import Laptops from "./componets/Laptops/Laptops.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Welcome to React Router</div>,
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "mobiles", Component: Mobiles },
+      { path: "laptops", Component: Laptops },
+    ],
   },
   {
     path: "about",
