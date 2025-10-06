c94e8951fd1c7bdf433e085c80458d2950a2d370
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-06 12:28:02 +0530

    add layout structure with header, sidebar, outlet and footer

---
+++ src/componets/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        <small>Thank you for visiting our website</small> <br></br>
+        <a href="/">Terms</a>
+        <a href="/">Privacy</a>
+        <a href="/fb">Facebook</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
       <h3>This is a header</h3>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/mobiles">Mobiles</Link>
+        <Link to="/laptop">Laptop</Link>
+      </nav>
     </div>
   );
 };
